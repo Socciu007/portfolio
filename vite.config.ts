e26850377130f09a751b154d32afb12ed4552e8a
VuTien <manhtien310701@gmail.com>
2025-03-09 15:38:50 +0700

    add optimize-project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false, // Disable source maps for production
+    chunkSizeWarningLimit: 600, // Increase the limit for the chunk size warning
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['react-router-dom', 'react-icons'],
+        },
+      },
+    },
+    minify: 'terser', // Minify the code
+    terserOptions: {
+      compress: {
+        drop_console: true, // Drop console.log
+        drop_debugger: true, // Drop debugger
+        ecma: 2020, // Optimize the code for the ECMAScript standard
+      },
+    },
+    cssCodeSplit: true, // Split the CSS code
+    target: 'esnext', // Target the latest ECMAScript standard
+  },
+  optimizeDeps: {
+    include: ['lodash'], // Pre-bundle the package
+  },
+  server: {
+    port: 3000,
+    host: true,
+    open: true
+  },
 })
