33e13b1b5642e0ceaa7655e86f33d37338653b6e
VuTien <manhtien310701@gmail.com>
2025-03-09 16:00:58 +0700

    add config-route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { routes } from './routes'
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={<route.component />} />
+        ))}
+      </Routes>
+    </Router>
   )
 }
 
