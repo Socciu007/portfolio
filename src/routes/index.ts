2c24e3a8ff26b2440fb348d5c7408dd6b7ed85e9
Money <manhtien310701@gmail.com>
2025-03-11 15:06:26 +0800

    update config-eslint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,15 @@
 import { lazy } from 'react'
 
 const HomePage = lazy(() => import('../pages/HomePage'))
+const ProjectDetailPage = lazy(() => import('../pages/ProjectDetailPage'))
 
 export const routes = [
   {
     path: '/',
     page: HomePage,
+  },
+  {
+    path: '/projects/:id',
+    page: ProjectDetailPage,
   },
 ]
